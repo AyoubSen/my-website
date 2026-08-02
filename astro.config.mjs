b104cf6eb902bba5a62c74d29c757e733cc1f071
AyoubSen <senraja0000@gmail.com>
2026-08-03 00:47:59 +0100

    logo fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ayoubsenhajihamim.netlify.app',
+	site: 'https://ayoubsenhajihamim.com',
 	integrations: [sitemap()],
 });
